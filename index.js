b9e0e8aee5a7da95d91c4a9fbc70c4a98b29090b
David Naranjo <dnaranjo89@gmail.com>
2019-10-22 19:33:52 +0200

    Notify Telegram group

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import asd from './scraper.js';
-const { scrape } = require('./scraper.js')
+const { checkAndNotify } = require('./scraper.js')
 const express = require('express')
 // const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -8,8 +8,8 @@ express()
     // .use(express.static(path.join(__dirname, 'public')))
     // .set('views', path.join(__dirname, 'views'))
     // .set('view engine', 'ejs')
-    .get('/', (req, res) => {
-        const response = scrape();
+    .get('/', async (req, res) => {
+        const response = await checkAndNotify();
         res.send(response)
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
--- scraper.js
+++ scraper.js
@@ -1,13 +1,27 @@
 const puppeteer = require('puppeteer');
+var request = require('request');
 
-const stringIsInPage =  async string => await page.evaluate(() => {
+const {
+    DEBUG_GROUP,
+    TARGET_GROUP,
+    TELEGRAM_BOT_TOKEN
+} = process.env;
+
+
+
+
+const stringIsInPage = async string => await page.evaluate(() => {
     const selector = 'body';
     return document.querySelector(selector).innerText.includes(string);
-  });
+});
 
-async function run() {
+
+
+async function checkIfTicketsAvailable() {
+    console.log('run');
+    let ticketsAvailable = false;
     const browser = await puppeteer.launch({
-        headless: false
+        headless: true
     });
     const page = await browser.newPage();
 
@@ -25,49 +39,57 @@ async function run() {
     await page.keyboard.type("Barcelona");
     await page.waitFor(500);
     page.keyboard.press('Enter');
-    
-    
+
+
     // Set Destination
     await page.click(destinationSelector);
     await page.keyboard.type("Sevilla");
     await page.waitFor(500);
     page.keyboard.press('Enter');
-    
+
     // Set date
     let searchInput = await page.$(dateSelector);
-    
-    await searchInput.click({clickCount: 3});
-    await searchInput.press('Backspace'); 
+
+    await searchInput.click({ clickCount: 3 });
+    await searchInput.press('Backspace');
     await page.keyboard.type("15/12/2019");
     await page.waitFor(500);
     page.keyboard.press('Enter');
-    
+
     // Submit
     await page.click(submitButtonSelector);
 
-    await page.waitFor(3*1000);
+    // Wait for page load
+    await page.waitFor(3 * 1000);
     try {
-
-    await page.waitForFunction(
-        `document.querySelector("body").innerText.includes("${stringNoResults}")`,
-        {timeout:3000}
+        await page.waitForFunction(
+            `document.querySelector("body").innerText.includes("${stringNoResults}")`,
+            { timeout: 3000 }
         );
-    }catch(e){
-        console.log('Results seem to be published', e);
-        await page.screenshot({path: 'page.png', fullPage:true});
-    }
-
         console.log('Results not public yet');
-        await browser.close();
-
-    browser.close();
+    } catch (e) {
+        ticketsAvailable = true;
+        console.log('Results seem to be published');
+        // await page.screenshot({ path: 'page.png', fullPage: true });
+    } finally {
+        browser.close();
+    }
+    return ticketsAvailable;
 }
 
-const sendNotification = () =>{
-
+const sendNotification = (ticketsAvailable) => {
+    const targetTelegramChannel = DEBUG_GROUP
+    const message = ticketsAvailable ? "Tickets available" : "Tickets not available"
+    request(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${targetTelegramChannel}&text=${message}`, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
 }
 
+const checkAndNotify = async () => {
+    const ticketsAvailable = await checkIfTicketsAvailable()
+    sendNotification(ticketsAvailable)
+}
 
-// run();
-const scrape = () => 'Yassss!';
-exports.scrape = scrape
+exports.checkAndNotify = checkAndNotify
